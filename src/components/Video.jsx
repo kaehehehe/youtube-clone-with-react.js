b25d37c6d769e38cf53b0ead72631aeacd2797e8
Kae <kaehehehe@gmail.com>
2022-02-20 13:09:51 +0900

    :sparkles: 인기있는 동영상에도 재생횟수가 표시되도록 구현

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { publishedAt } from '../logic/publishedAt';
+import { convertNumber } from '../logic/convertNumber';
+import { VscDebugStackframeDot } from 'react-icons/vsc';
 
 const StyledVideo = styled.li`
   transition: all 250ms ease-in;
   cursor: pointer;
-  
+
   &:hover {
     transform: scale(1.05);
   }
@@ -18,6 +20,13 @@ const Thumbnail = styled.img`
 const Wrapper = styled.div`
   display: flex;
   margin-top: 8px;
+`;
+
+const ChannelThumbnail = styled.img`
+  width: 36px;
+  height: 36px;
+  border-radius: 50%;
+  margin-right: 13px;
 `;
 
 const VideoMetadata = styled.div`
@@ -36,30 +45,36 @@ const VideoMetadata = styled.div`
   }
 
   .published-at {
-    margin-top: 3px;
   }
 `;
 
-const ChannelThumbnail = styled.img`
-  width: 36px;
-  height: 36px;
-  border-radius: 50%;
-  margin-right: 13px;
+const MetadataWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 12px;
+  margin-top: 3px;
+  color: var(--gray-color);
+
+  .published-at {
+    margin-left: 3px;
+  }
 `;
 
 const Video = ({ video, handleSelectedVideo }) => {
   const [channel, setChannel] = useState(null);
+  const [viewCount, setViewCount] = useState(null);
   const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
   const getRequestOptions = { method: 'GET', redirect: 'follow' };
 
   useEffect(() => {
     fetch(
-      `https://youtube.googleapis.com/youtube/v3/channels?part=snippet&id=${video.snippet.channelId}&key=${API_KEY}`,
+      `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${video.snippet.channelId}&key=${API_KEY}`,
       getRequestOptions
     )
       .then((res) => res.json())
       .then((res) => {
         setChannel(res.items[0].snippet.thumbnails.default.url);
+        setViewCount(res.items[0].statistics.viewCount);
       });
   }, []);
 
@@ -74,9 +89,13 @@ const Video = ({ video, handleSelectedVideo }) => {
         <VideoMetadata>
           <p className="title">{video.snippet.title}</p>
           <p className="channel-name">{video.snippet.channelTitle}</p>
-          <p className="published-at">
-            {publishedAt(video.snippet.publishedAt)} ago
-          </p>
+          <MetadataWrapper>
+            <span>{`${convertNumber(viewCount)} views`}</span>
+            <VscDebugStackframeDot color="gray" />
+            <span className="published-at">
+              {publishedAt(video.snippet.publishedAt)} ago
+            </span>
+          </MetadataWrapper>
         </VideoMetadata>
       </Wrapper>
     </StyledVideo>

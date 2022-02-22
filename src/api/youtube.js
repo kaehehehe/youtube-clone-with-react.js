cee5056dcd7bd52894344657f4344c765c8c5de0
Kae <kaehehehe@gmail.com>
2022-02-22 14:06:01 +0900

    :adhesive_bandage: async 키워드 추가

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -4,46 +4,44 @@ class Youtube {
     this.getRequestOptions = { method: 'GET', redirect: 'follow' };
   }
 
-  getMostPopularVideos() {
-    return fetch(
+  async getMostPopularVideos() {
+    const response = await fetch(
       `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=KR&key=${this.key}`,
       this.getRequestOptions
-    )
-      .then((res) => res.json())
-      .then((data) => data.items);
+    );
+    const data = await response.json();
+    return data.items;
   }
 
-  getSearchVideos(inputValue) {
-    return fetch(
+  async getSearchVideos(inputValue) {
+    const response = await fetch(
       `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=${inputValue}&type=video&key=${this.key}`,
       this.getRequestOptions
-    )
-      .then((res) => res.json())
-      .then((data) =>
-        data.items.map((item) => ({
-          ...item,
-          id: item.id.videoId,
-        }))
-      );
+    );
+    const data = await response.json();
+    return data.items.map((item) => ({
+      ...item,
+      id: item.id.videoId,
+    }));
   }
 
-  getChannelsData(video) {
+  async getChannelsData(video) {
     const { channelId } = video.snippet;
-    return fetch(
+    const response = await fetch(
       `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${channelId}&key=${this.key}`,
       this.getRequestOptions
-    )
-      .then((res) => res.json())
-      .then((data) => data.items);
+    );
+    const data = await response.json();
+    return data.items;
   }
 
-  getCommentsData(video) {
-    return fetch(
+  async getCommentsData(video) {
+    const response = await fetch(
       `https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet&videoId=${video.id}&maxResults=10&key=${this.key}`,
       this.getRequestOptions
-    )
-      .then((res) => res.json())
-      .then((data) => data.items);
+    );
+    const data = await response.json();
+    return data.items;
   }
 }
 
